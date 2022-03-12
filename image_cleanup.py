56237cf79b909373c6722358040936980ac86199
whimsicalwill <knipewilliam20@gmail.com>
2022-03-12 02:09:47 -0600

    Don't resize images that are too small

--- image_cleanup.py
+++ image_cleanup.py
@@ -8,7 +8,7 @@ import numpy as np
 import cv2
 
 imgs = []
-path = "./pix_data"
+path = "./source_data"
 valid_exts = [".jpg", ".png"]
 invalid_count, valid_count = 0, 0
 for i, f in enumerate(os.listdir(path)):
@@ -17,11 +17,15 @@ for i, f in enumerate(os.listdir(path)):
     ext = split_path[1]
     if ext.lower() not in valid_exts: # if its not valid
         print(f"Image {i} not valid")
-        os.remove() # remove from dir
+        os.remove(img_path) # remove from dir
         invalid_count += 1 # increment num removed
         continue
     else: # if the image is valid
         img = cv2.imread(img_path) # load image
+
+        # skip if image too small
+        if img.shape[0] < 256 or img.shape[1] < 256:
+            continue
 
         # use opencv to count white pixels
         img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -32,7 +36,7 @@ for i, f in enumerate(os.listdir(path)):
         # shrink image and save to new dir
         dh, dw =  img.shape[0], img.shape[1] // 2 # new img dimensions (shrink width)
         res = cv2.resize(img, dsize=(dw, dh), interpolation=cv2.INTER_CUBIC)   
-        cv2.imwrite(f"./resized_pix_data/pix_image{str(i).zfill(4)}.png", res)
+        cv2.imwrite(f"./resized_source_data/source_data{str(i).zfill(4)}.png", res)
         valid_count += 1
 
 print(f"Invalid count: {invalid_count}")
--- image_scraper.py
+++ image_scraper.py
@@ -2,8 +2,8 @@
 from google_images_download import google_images_download
 
 chromedriver_str = r"/usr/bin/chromedriver"
-prompt = "Aesthetic pixel art landscape"
+prompt = "ancient ruins landscape professional photography"
 response = google_images_download.googleimagesdownload()
-arguments = {"keywords": prompt, "limit": "5000", "print_urls": False, "chromedriver": chromedriver_str, "size": ">640*480"}
+arguments = {"keywords": prompt, "limit": "6000", "print_urls": False, "chromedriver": chromedriver_str, "size": ">640*480"}
 paths = response.download(arguments)
 print(paths)
