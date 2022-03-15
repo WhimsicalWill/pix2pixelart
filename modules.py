9d2f032612c43cced84a529335fa51fa869f4e0f
whimsicalwill <knipewilliam20@gmail.com>
2022-03-15 17:02:53 -0400

    Add TravelGAN files

---
+++ modules.py
@@ -0,0 +1,148 @@
+import torch 
+import torch.nn as nn
+import torch.nn.functional as F 
+import numpy as np
+
+from utils import weights_init
+from collections import OrderedDict
+from itertools import combinations
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_feat, num_feat, reduction=16, attention=False):
+        super().__init__()
+        layers = []
+        layers.append(nn.Conv2d(in_feat, num_feat, kernel_size=3, padding=1, bias=False))
+        layers.append(nn.BatchNorm2d(num_feat))
+        layers.append(nn.ReLU(inplace=True))
+        layers.append(nn.Conv2d(num_feat, in_feat, kernel_size=3, padding=1, bias=False))
+        layers.append(nn.BatchNorm2d(in_feat))
+        self.main = nn.Sequential(*layers)
+        self.is_attention = attention
+        if attention :
+            attentionlayer = [] 
+            attentionlayer.append(nn.AdaptiveAvgPool2d(1))
+            attentionlayer.append(nn.Conv2d(in_feat, in_feat//reduction, kernel_size=1))
+            attentionlayer.append(nn.ReLU(inplace=True))
+            attentionlayer.append(nn.Conv2d(in_feat//reduction, in_feat, kernel_size=1))
+            attentionlayer.append(nn.Sigmoid())
+            self.attention = nn.Sequential(*attentionlayer)
+
+    def forward(self, x):
+        out = self.main(x)
+        if self.is_attention :
+            x = x + out * self.attention(out)
+        else :
+            x = x + out 
+        return x
+
+
+class Generator(nn.Module):
+    def __init__(self, in_channels, num_feat, num_res):
+        super().__init__()
+
+        layers = []
+        layers.append(nn.Conv2d(in_channels, num_feat, kernel_size=7, stride=1, padding=3, bias=False))
+        layers.append(nn.ReLU(inplace=True))
+
+        curr_dim = num_feat
+        for _ in range(2):
+            layers.append(nn.Conv2d(curr_dim, curr_dim*2, kernel_size=4, stride=2, padding=1, bias=False))
+            layers.append(nn.BatchNorm2d(curr_dim*2))
+            layers.append(nn.ReLU(inplace=True))
+            curr_dim = curr_dim * 2
+
+        
+        for _ in range(num_res):
+            layers.append(ResidualBlock(curr_dim, curr_dim))
+
+
+        for _ in range(2):
+            layers.append(nn.ConvTranspose2d(curr_dim, curr_dim//2, kernel_size=4, stride=2, padding=1, bias=False))
+            layers.append(nn.BatchNorm2d(curr_dim//2))
+            layers.append(nn.ReLU(inplace=True))
+            curr_dim = curr_dim // 2
+        layers.append(nn.Conv2d(curr_dim, curr_dim, kernel_size=3, stride=1, padding=1, bias=False))
+        layers.append(nn.BatchNorm2d(curr_dim))
+        layers.append(nn.ReLU(inplace=True))
+        layers.append(nn.Conv2d(curr_dim, in_channels, kernel_size=7, stride=1, padding=3, bias=False))
+        layers.append(nn.Tanh())
+        
+        self.main = nn.Sequential(*layers)
+        self.apply(weights_init())
+
+    def forward(self, x):
+        return self.main(x)
+
+
+class Discriminator(nn.Module):
+    """Discriminator network with PatchGAN."""
+    def __init__(self, in_channels, num_feat=64, num_repeat=6):
+        super().__init__()
+        layers = []
+        layers.append(nn.Conv2d(in_channels, num_feat, kernel_size=4, stride=2, padding=1))
+        layers.append(nn.LeakyReLU(0.02, inplace=True))
+
+        curr_dim = num_feat
+        for _ in range(1, num_repeat):
+            layers.append(nn.Conv2d(curr_dim, curr_dim*2, kernel_size=4, stride=2, padding=1))
+            layers.append(nn.LeakyReLU(0.02, inplace=True))
+            curr_dim = curr_dim * 2
+
+        self.main = nn.Sequential(*layers)
+        self.conv1 = nn.Conv2d(curr_dim, 1, kernel_size=3, stride=1, padding=1)
+        self.apply(weights_init())
+
+    def forward(self, x):
+        h = self.main(x)
+        out = self.conv1(h)
+        return out
+
+    def calc_dis_loss(self, x_real, x_fake):
+        real_pred = self.forward(x_real)
+        fake_pred = self.forward(x_fake)
+        loss = torch.mean((real_pred - 1)**2) + torch.mean((fake_pred - 0)**2)
+        return loss
+    
+    def calc_gen_loss(self, x):
+        pred = self.forward(x)
+        loss = torch.mean((pred - 1)**2)
+        return loss 
+
+class SiameseNet(nn.Module):
+    def __init__(self, image_size, in_channels, num_feat=64, num_repeat=5, gamma=10):
+        super().__init__()
+        layers = []
+        self.gamma = gamma 
+        layers.append(nn.Conv2d(in_channels, num_feat, kernel_size=4, stride=2, padding=1))
+        layers.append(nn.LeakyReLU(0.02, inplace=True))
+
+        curr_dim = num_feat
+        for _ in range(1, num_repeat):
+            layers.append(nn.Conv2d(curr_dim, curr_dim*2, kernel_size=4, stride=2, padding=1))
+            layers.append(nn.LeakyReLU(0.02, inplace=True))
+            curr_dim = curr_dim * 2
+
+        in_feat = image_size // 2**(num_repeat)
+
+        self.main = nn.Sequential(*layers)
+        self.linear = nn.Linear(curr_dim*in_feat**2, 1024)
+        self.apply(weights_init())
+
+    def _forward(self, x1, x2):
+        latent1 = self.main(x1)
+        latent2 = self.main(x2)
+        latent1 = self.linear(latent1.flatten(1))
+        latent2 = self.linear(latent2.flatten(1))
+        return latent1, latent2
+        
+    def calc_loss(self, x1, x2):
+        pairs = np.asarray(list(combinations(list(range(x1.size(0))), 2)))
+        latent1, latent2 = self._forward(x1, x2)
+        v1 = latent1[pairs[:,0]] - latent1[pairs[:,1]]
+        v2 = latent2[pairs[:,0]] - latent2[pairs[:,1]]
+        distance = F.mse_loss(v1, v2) - torch.mean(F.cosine_similarity(v1, v2))
+        margin = self.margin_loss(v1)
+        return distance + margin
+
+    def margin_loss(self, v1):
+        return torch.mean(F.relu(10 - torch.norm(v1, dim=1)))
---
+++ travelgan.py
@@ -0,0 +1,115 @@
+import torch 
+import torch.nn as nn 
+import torch.nn.functional as F 
+import torch.optim as optim 
+import wandb 
+
+from src.modules import Generator, Discriminator, SiameseNet
+from tqdm import tqdm
+
+
+wandb.init('TravelGAN', name='VanGogh')
+
+class TravelGan:
+    def __init__(self, config, logger):
+        self.logger = logger 
+        self.config = config 
+        self.device = self.config['device']
+        self.dis = Discriminator(config['in_channels'], num_feat=config['num_feat'], num_repeat=config['num_repeat']).to(self.device)
+        self.gen = Generator(config['in_channels'], num_feat=config['num_feat'], num_res=config['num_res']).to(self.device)
+        self.siamese = SiameseNet(config['image_size'], config['in_channels'], num_feat=config['num_feat'], 
+                                  num_repeat=config['num_repeat'], gamma=config['gamma']).to(self.device)
+        wandb.watch([self.gen, self.dis, self.siamese], log='all')
+        
+        #optimizer
+        gen_params = list(self.gen.parameters()) + list(self.siamese.parameters())
+        self.opt_gen = optim.Adam(gen_params, lr=config['gen_lr'], betas=(config['gbeta1'], config['gbeta2']))
+        self.opt_dis = optim.Adam(self.dis.parameters(), lr=config['dis_lr'], betas=(config['dbeta1'], config['dbeta2']))
+        
+        # scheduler 
+        self.gen_scheduler = optim.lr_scheduler.StepLR(self.opt_gen, config['step_size'], gamma=0.1)
+        self.dis_scheduler = optim.lr_scheduler.StepLR(self.opt_dis, config['step_size'], gamma=0.1)
+
+    
+    def _train_epoch(self, loaderA, loaderB, epoch):
+        for i, (x_a, x_b) in enumerate(zip(loaderA, loaderB)):
+            global_step = len(loaderB) * epoch + i
+            
+            if isinstance(x_a, (tuple, list)):
+                x_a = x_a[0]
+            if isinstance(x_b, (tuple, list)):
+                x_b = x_b[0]
+            
+            x_a = x_a.to(self.device)
+            x_b = x_b.to(self.device)
+            
+            #===============================
+            # Dis Update 
+            #===============================
+            self.opt_dis.zero_grad()
+            x_ab = self.gen(x_a)
+            dis_loss = self.dis.calc_dis_loss(x_b, x_ab.detach())
+            dis_loss.backward()
+            self.opt_dis.step()
+            
+            if global_step  % self.config['iter_log'] == 0:
+                self.logger.add_scalar('dis_loss', dis_loss.item(), global_step)
+
+            #===============================
+            # Gen Update 
+            #===============================
+            self.opt_gen.zero_grad()
+            gen_adv_loss = self.dis.calc_gen_loss(x_ab)
+            gen_siamese_loss = self.siamese.calc_loss(x_a, x_ab)
+
+            gen_loss = self.config['gen_adv_loss_w'] * gen_adv_loss + \
+                       self.config['siamese_loss_w'] * gen_siamese_loss
+            
+            gen_loss.backward()
+            self.opt_gen.step()
+            
+            if global_step % self.config['iter_log'] == 0 :
+                self.logger.add_scalar('gen_loss', gen_loss.item(), global_step)
+                self.logger.add_scalar('gen_adv_loss', gen_adv_loss.item(), global_step)
+                self.logger.add_scalar('siamese_loss', gen_siamese_loss.item(), global_step)
+            
+            if global_step % self.config['iter_sample'] == 0:
+                self.sample(x_a, global_step)
+            
+            
+    
+    def train(self, loaderA, loaderB):
+        for i in tqdm(range(self.config['epochs'])):
+            self.gen.train()
+            self._train_epoch(loaderA, loaderB, i)
+            self.gen_scheduler.step()
+            self.dis_scheduler.step()
+            
+            if i % self.config['checkpoint_iter'] == 0:
+                self.save(i)
+
+    def sample(self, x_a, step):
+        self.gen.eval()
+        x_ab = self.gen(x_a)
+        self.logger.add_image('real images', x_a, step)
+        self.logger.add_image('sampled images', x_ab, step)
+
+    def save(self, iter): 
+        torch.save({'gen' : self.gen.state_dict(),
+                    'dis' : self.dis.state_dict(),
+                    'siamese' : self.siamese.state_dict(),
+                    'gen_opt' : self.opt_gen.state_dict(),
+                    'dis_opt' : self.opt_dis.state_dict(),
+                    'gen_scheduler' : self.gen_scheduler.state_dict(),
+                    'dis_scheduler' : self.dis_scheduler.state_dict()
+        }, f'logs/checkpoints/checkpoint_n{iter}.pt')
+        
+    def load(self):
+        checkpoint = torch.load(self.config['checkpoint_path'])
+        self.gen.load_state_dict(checkpoint['gen'])
+        self.dis.load_state_dict(checkpoint['dis'])
+        self.siamese.load_state_dict(checkpoint['siamese'])
+        self.opt_dis.load_state_dict(checkpoint['dis_opt'])
+        self.opt_gen.load_state_dict(checkpoint['gen_opt'])
+        self.gen_scheduler.load_state_dict(checkpoint['gen_scheduler'])
+        self.dis_scheduler.load_state_dict(checkpoint['dis_scheduler'])
