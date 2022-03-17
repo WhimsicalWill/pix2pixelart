da49a28255919823d3fc3752f5ab8dcf209d5bd0
whimsicalwill <knipewilliam20@gmail.com>
2022-03-17 01:24:07 -0400

    Update cleanup and scraper helper files

--- image_scraper.py
+++
@@ -1,9 +0,0 @@
-# scrape images using google-images-downloader package
-from google_images_download import google_images_download
-
-chromedriver_str = r"/usr/bin/chromedriver"
-prompt = "ancient ruins landscape professional photography"
-response = google_images_download.googleimagesdownload()
-arguments = {"keywords": prompt, "limit": "6000", "print_urls": False, "chromedriver": chromedriver_str, "size": ">640*480"}
-paths = response.download(arguments)
-print(paths)
